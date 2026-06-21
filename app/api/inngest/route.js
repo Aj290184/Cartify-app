19de5aa866bb907b126ea68ad4a85f58b90cbf57
Ajay Kumar <abharoja@gmail.com>
2026-06-21 13:18:03 +0530

    implement Clerk webhook integration

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,5 @@
-// src/app/api/inngest/route.ts
 import { serve } from "inngest/next";
-import { creatUserOrder } from "@/config/inngest.js";
+import { creatUserOrder, inngest } from "@/config/inngest.js";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
